bdbdf0c080541f51207ab4a10d428cc245c688c1
Mitchell Tishmack <git@mitchty.net>
2022-07-16 19:31:09 -0500

    checkpoint

--- flake.nix
+++ flake.nix
@@ -15,70 +15,10 @@
           (import rust)
         ];
       };
-
-      inherit (pkgs) fetchFromGitHub;
-      inherit (pkgs.stdenv) mkDerivation;
-
-      # TODO: Why does calling this via callPackage no worky? Future me figure
-      # out if you get a bug up your butt to do so this works for now so
-      # whatever.
-      llvm-xtensa = mkDerivation rec {
-        name = "llvm-xtensa";
-        version = "esp-14.0.0-20220415";
-
-        target = "Xtensa";
-
-        src = fetchFromGitHub {
-          owner = "espressif";
-          repo = "llvm-project";
-          rev = "${version}";
-          fetchSubmodules = true;
-          leaveDotGit = true;
-          sha256 = "sha256-FpUkLSSwCuojZev7+QWDD9KPYmpnxKqrJzNsLrVfkpQ=";
-        };
-
-        buildInputs = with pkgs; [
-          clang
-          git
-          python3
-          cmake
-          ninja
-          llvm
-        ];
-
-        phases = [
-          "unpackPhase"
-          "buildPhase"
-          "installPhase"
-          "fixupPhase"
-        ];
-
-        # TODO: set this up a bit better, for now hacks is fine, builds at level 1 is sloooooow.
-        CMAKE_BUILD_PARALLEL_LEVEL = "8";
-
-        # http://quickhack.net/nom/blog/2019-05-14-build-rust-environment-for-esp32.html
-        buildPhase = ''
-          install -d build
-          cd build
-
-          cmake -S ../llvm -D LLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" -D LLVM_BUILD_LLVM_DYLIB= -D LLVM_EXPERIMENTAL_TARGETS_TO_BUILD=${target} -D LLVM_TARGETS_TO_BUILD=${target} -D CMAKE_BUILD_TYPE=Release
-
-          cmake --build .
-        '';
-
-        installPhase = ''
-          mkdir -p $out
-          cmake -DCMAKE_INSTALL_PREFIX=$out -P cmake_install.cmake
-        '';
-
-        meta.mainProgram = "clang";
-      };
     in
     rec {
       packages = flake-utils.lib.flattenTree {
-        inherit llvm-xtensa;
-        # TODO: figure out why callpackage no worky with fetchfromgithub like ^^^
-        # llvm-xtensa = pkgs.callPackage ./packages/llvm-xtensa.nix { src = llvmsrc; };
+        llvm-xtensa = pkgs.callPackage ./packages/llvm-xtensa.nix { };
         esp-idf = pkgs.callPackage ./packages/esp-idf.nix { };
       };
 
@@ -132,17 +72,20 @@
           ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
           install -dm755 $out
         '';
+        # TODO: Build binutils as a cross compile chain and assemble the .S file
+        # clang built to see if we're kosher.
         llvm-xtensa = pkgs.runCommand "check-llvm-xtensa" { } ''
-          asmfile=$(${pkgs.coreutils}/bin/mktemp /tmp/llvm-xtensa.S-XXXXXXXXX)
+          asmfile=$(${pkgs.coreutils}/bin/mktemp /tmp/XXXXXXXX-llvm-xtensa.S)
           cleanup() { rm -f $asmfile > /dev/null 2>&1; }
           trap cleanup EXIT
           ${packages.llvm-xtensa}/bin/clang -target xtensa -fomit-frame-pointer -S ${./checks/test.c} -o $asmfile
           install -dm755 $out
         '';
-        # TODO: what to do here...
+        # TODO: what to do here... to basic unit test...
         esp-idf = pkgs.runCommand "check-esp-idf" { } ''
           install -dm755 $out
         '';
+        # TODO: rust..
         # rustc-xtensa = pkgs.runCommand "check-rustc-xtensa" { } ''
         #   ${rust-xtensa}/bin/rustc --version
         #   install -dm755 $out
@@ -150,98 +93,3 @@
       };
     });
 }
-
-# Cargo/rust from the overlay used for bootstrapping/builds
-# rust-xtensa = with pkgs; (makeRustPlatform {
-#   cargo = rust-bin.stable.latest.minimal;
-#   rustc = rust-bin.stable.latest.minimal;
-# }).buildRustPackage rec {
-#   pname = "rust-xtensa";
-#   version = "0.3.7";
-
-#   src = fetchFromGitHub {
-#     owner = "blacknon";
-#     repo = pname;
-#     rev = version;
-#     sha256 = "sha256-FVqvwqsHkV/yK5okL1p6TiNUGDK2ZnzVNO4UDVkG+zM=";
-#     forceFetchGit = true;
-#   };
-
-#   # Update via regenpatches
-#   cargoPatches = [
-#     ./patches/hwatch-add-cargo-lock.patch
-#   ];
-
-#   cargoSha256 = "sha256-kfn7iOREFVS9LttfeRu+z5tXCheg54+tYozTsteFOX0=";
-
-#   passthru.tests.version = testVersion { package = hwatch; };
-
-#   latest = "curl --silent 'https://api.github.com/repos/blacknon/hwatch/releases/latest' | jq -r '.tag_name'";
-# };
-# rust-xtensa = with pkgs; rustc.overrideAttrs (old: rec {
-# pname = "rustc-xtensa";
-
-# Note not using fetchFromGitHub as the base derivation gets a tar.gz
-# file and unpack phase and patch expect/are written to expect that.
-# rust-src = fetchFromGitHub {
-#   owner = "esp-rs";
-#   repo = "rust";
-#   rev = version;
-#   sha256 = "sha256-g92H324NAO4LG05Gl0hNGLh+V7dKZw5oZOJHXtFpquE=";
-#   fetchSubmodules = true;
-# };
-
-#     src = fetchurl {
-#       url = "https://github.com/esp-rs/rust/archive/refs/heads/esp-1.61.0.0.tar.gz";
-#       sha256 = "sha256-UZCBpMHSv000w+zze1KBPhtMz0lm8Lvfrs71ShD0FVM=";
-#     };
-
-#     cargo = rust-bin.stable.latest.minimal;
-#     rustc = rust-bin.stable.latest.minimal;
-#     version = "esp-1.61.0.0";
-#     llvmSharedForBuild = llvm-xtensa;
-#     llvmSharedForHost = llvm-xtensa;
-#     llvmSharedForTarget = llvm-xtensa;
-#     llvmShared = llvm-xtensa;
-
-#     configureFlags =
-#       (lib.lists.remove "--enable-llvm-link-shared" old.configureFlags
-#         # (lib.lists.remove "--release-channel=stable" old.configureFlags)
-#       ) ++ [
-#         "--llvm-root=${llvm-xtensa}"
-#         "--experimental-targets=Xtensa"
-#         # "--release-channel=nightly"
-#       ];
-
-#     # postUnpack = ''
-#     #   ${old.postUnpack}
-#     # '';
-
-#     # patchPhase = ''
-#     #   set +x
-#     #   ${old.p}
-#     # '';
-
-#     postConfigure = ''
-#       ${old.postConfigure}
-#       unpackFile "$cargoDeps"
-#       mv $(stripHash $cargoDeps) vendor
-#     '';
-
-#     postInstall = ''
-#       ${old.postInstall}
-#       install -d $out/lib/rustlib/src
-#       ln -s $src $out/lib/rustlib/src/rust
-#       install -d $out/vendor
-#       ln -s $src/library/rustc-std-workspace-alloc $out/vendor/rustc-std-workspace-alloc
-#       ln -s $src/library/rustc-std-workspace-core $out/vendor/rustc-std-workspace-core
-#       ln -s $src/library/rustc-std-workspace-std $out/vendor/rustc-std-workspace-std
-#     '';
-#   });
-# in
-# rec {
-#   packages = flake-utils.lib.flattenTree {
-#     inherit llvm-xtensa;
-#     inherit esp-idf; # needed?
-#     inherit rust-xtensa;
-#   };
--- packages/llvm-xtensa.nix
+++ packages/llvm-xtensa.nix
@@ -6,48 +6,53 @@
 
 stdenv.mkDerivation rec {
   name = "llvm-xtensa";
-  version = "14.0.0";
+  version = "esp-14.0.0-20220415";
 
-  inherit src;
+  target = "Xtensa";
 
-  # src = pkgs.fetchFromGitHub {
-  #   owner = "espressif";
-  #   repo = "llvm-project";
-  #   #   # rev = "esp-14.0.0-20220415";
-  #   #   # sha256 = "sha256-FpUkLSSwCuojZev7+QWDD9KPYmpnxKqrJzNsLrVfkpQ=";
-  #   rev = "xtensa_release_14.0.0";
-  #   sha256 = lib.fakeSha256;
-  #   #   rev = "esp-14.0.0-20220415";
-  #   #   sha256 = "sha256-d83JWsj6snhSf5wpJ5/8NYep8C8+SLOvI9jynEPWiCo=";
-  #   fetchSubmodules = true;
-  #   #   leaveDotGit = true;
-  # };
+  src = pkgs.fetchFromGitHub {
+    owner = "espressif";
+    repo = "llvm-project";
+    rev = "${version}";
+    fetchSubmodules = true;
+    leaveDotGit = true;
+    sha256 = "sha256-FpUkLSSwCuojZev7+QWDD9KPYmpnxKqrJzNsLrVfkpQ=";
+  };
 
-  # buildInputs = lib.attrVals [
-  #   "python3"
-  #   "cmake"
-  #   "ninja"
-  # ]
-  #   pkgs;
   buildInputs = with pkgs; [
+    clang
     git
     python3
     cmake
     ninja
+    llvm
   ];
 
-  sourceRoot = "source/llvm";
-
-  target = "Xtensa";
-  cmakeFlags = [
-    "-DLLVM_ENABLE_PROJECTS=clang"
-    "-DLLVM_BUILD_DYLIB="
-    "-DLLVM_INSTALL_UTILS=ON"
-    "-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=${target}"
-    "-DLLVM_TARGETS_TO_BUILD=${target}"
-    "-DLLVM_ENABLE_RUNTIMES=libcxx"
-    "-DCMAKE_BUILD_TYPE=Release"
+  phases = [
+    "unpackPhase"
+    "buildPhase"
+    "installPhase"
+    "fixupPhase"
   ];
+
+  # TODO: set this up a bit better, for now hacks is fine, builds at level 1 is
+  # sloooooow, beat the piss out of the cpus i'm using for now.
+  CMAKE_BUILD_PARALLEL_LEVEL = "8";
+
+  # http://quickhack.net/nom/blog/2019-05-14-build-rust-environment-for-esp32.html
+  buildPhase = ''
+    install -d build
+    cd build
+
+    cmake -S ../llvm -D LLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" -D LLVM_BUILD_LLVM_DYLIB= -D LLVM_EXPERIMENTAL_TARGETS_TO_BUILD=${target} -D LLVM_TARGETS_TO_BUILD=${target} -D CMAKE_BUILD_TYPE=Release
+
+    cmake --build .
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cmake -DCMAKE_INSTALL_PREFIX=$out -P cmake_install.cmake
+  '';
 
   meta = {
     mainProgram = "clang";
@@ -55,47 +60,4 @@ stdenv.mkDerivation rec {
     homepage = https://github.com/espressif/llvm-project;
     license = lib.licenses.asl20;
   };
-
-  # passthru.tests.version = stdenv.testVersion { package = llvm-xtensa; };
 }
-# stdenv.mkDerivation rec {
-#         name = "llvm-xtensa";
-#         version = "";
-
-#         target = "Xtensa";
-
-#         };
-
-#         buildInputs = [
-#           clang
-#           git
-#           python3
-#           cmake
-#           ninja
-#           llvm
-#         ];
-
-#         phases = [
-#           "unpackPhase"
-#           "buildPhase"
-#           "installPhase"
-#           "fixupPhase"
-#         ];
-
-#         # http://quickhack.net/nom/blog/2019-05-14-build-rust-environment-for-esp32.html
-#         buildPhase = ''
-#           install -d build
-#           cd build
-
-#           cmake -S ../llvm -D LLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" -D LLVM_BUILD_LLVM_DYLIB= -D  -D LLVM_TARGETS_TO_BUILD=${target} -D CMAKE_BUILD_TYPE=Release
-
-#           cmake --build .
-#         '';
-
-#         installPhase = ''
-#           mkdir -p $out
-#           cmake -DCMAKE_INSTALL_PREFIX=$out -P cmake_install.cmake
-#         '';
-
-#         meta.mainProgram = "clang";
-#       };
